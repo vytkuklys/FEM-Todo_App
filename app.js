04d1276b18eac4c725afb5db871d21fb7200b08c
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2021-01-11 06:42:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// localStorage.clear()
-const switchs = document.querySelector(".switch__h1");
-switchs.addEventListener("click", ()=>{
-    localStorage.clear();
-    console.log("as")
-})
 //---------SELECT ELEMENTS----------
 const btnClear = document.querySelector(".list-todo__btn-clear");
 const btnSwitch = document.querySelector(".switch__btn");
@@ -25,12 +19,10 @@ const LIGHT_MODE = "light-mode";
 const BODY_BG = "light-mode__body";
 const FONTACTIVE = "font-active"
 
-let idArr = [];
-
-//load local storage
+//-----------Load local storage
 let id = 0,
     LIST = [],
-    idea = "";
+    task = "";
 let data = localStorage.getItem("todoList");
 if (data) {
     LIST = JSON.parse(data);
@@ -39,27 +31,25 @@ if (data) {
 } else {
     LIST = [];
     id = 0;
-    while(id < 4){
-        console.log(id)
-        switch(id){
+    while (id < 4) {
+        switch (id) {
             case 0:
-                idea = "Display: Gratitude;";
+                task = "Show gratitude";
                 break;
             case 1:
-                idea = "Contemplate: Death;";
+                task = "Comtemplate imminence of death";
                 break;
             case 2:
-                idea = "Drink: 2 liter of water;";
+                task = "Drink 2 liters of water";
                 break;
             case 3:
-                idea = "Sleep: Solid 8 hours straight;";
+                task = "Sleep for solid 8 hours straight";
                 break;
         }
         LIST.push({
-            name: idea,
+            name: task,
             id: id,
-            completed: false,
-            deleted: false
+            completed: false
         });
         id++;
     }
@@ -68,23 +58,15 @@ if (data) {
     counter();
 }
 
-/*draggables is placed below local storage to avoid errors*/
+/*These variables are placed below local storage to avoid errors*/
 let draggables = document.querySelectorAll('.draggable');
 let listItems = document.querySelectorAll(".list-todo__li");
 
 /*----------EVENT LISTENERS---------------*/
 
-/*-----mouseover and mouseout event to add/remove delete icon*/
-draggables.forEach((draggable) => {
-    draggable.addEventListener("mouseover", () => {
-        draggable.classList.remove("mouseover")
-    })
-})
-draggables.forEach((draggable) => {
-    draggable.addEventListener("mouseout", () => {
-        draggable.classList.add("mouseover")
-    })
-})
+/*Dragover/dragend and Mouseover/Mouseleave envent listeners where added to a function for the lack
+of better way to sustain their usability after interaction (changes to list items)*/
+refreshEventListeners();
 
 /*click event to run delete or complete function*/
 listTodo.addEventListener("click", (event) => {
@@ -101,60 +83,61 @@ listTodo.addEventListener("click", (event) => {
 });
 
 /*click event to run a function for filter to show all items*/
-btnAll.forEach((btn)=>{
+btnAll.forEach((btn) => {
     btn.addEventListener("click", function dog() {
-        let allArr = LIST.filter((item) => (item.deleted == false));
         emptyList();
-        loadList(allArr);
-        idArr = [];
-        // getIdArr(allArr);
-        refreshDraggablesEvent();
+        loadList(LIST);
+        refreshEventListeners();
         changeFont("btnAll");
     });
 });
-// function getIdArr(arr){
-//     let i = 0;
-//     arr.forEach((item)=>{
-//         idArr[i] = item.id;
-//         i++;
-//     })
-//     console.log(idArr, "idArr")
-// }
+
 /*click event to run a function for filter to show active/incomplete items*/
-btnActive.forEach((btn)=>{
+btnActive.forEach((btn) => {
     btn.addEventListener("click", () => {
-        let activeArr = LIST.filter((item) => (item.completed == false && item.deleted == false));
+        let activeArr = LIST.filter((item) => (item.completed == false));
         emptyList();
         changeFont("btnActive");
         loadList(activeArr);
-        // idArr = [];
-        // getIdArr(activeArr);
-        refreshDraggablesEvent();
+        refreshEventListeners();
     });
 });
 
 
 /*click event to run a function for filter to show completed items*/
-btnComplete.forEach((btn)=>{
+btnComplete.forEach((btn) => {
     btn.addEventListener("click", () => {
         let completeArr = LIST.filter((item) => (item.completed == true));
         emptyList();
         changeFont("btnComplete");
         loadList(completeArr);
-        // idArr = [];
-        // getIdArr(completeArr);
-        refreshDraggablesEvent();
+        refreshEventListeners();
     });
 });
 
 /*click event to run a function to remove completed items*/
 btnClear.addEventListener("click", () => {
-    let clearItem = LIST.filter((item) => (item.completed == true && item.deleted == false));
-    let elementz;
-    clearItem.forEach((item) => {
-        elementz = document.getElementById(item.id);
-        deleteTodo(elementz);
+    let completedItems = LIST.filter((item) => (item.completed == true));
+    let completed;
+    let toRemoveFromLocalStorage = [];
+    let i = 0;
+    completedItems.forEach((item) => {
+        completed = document.getElementById(item.id);
+        completed.parentNode.parentNode.removeChild(completed.parentNode);
+        toRemoveFromLocalStorage[i] = completed.id;
+        i++;
     });
+    //for loop to filter out removed items from local storage
+    for (let j = 0; j < toRemoveFromLocalStorage.length; j++) {
+        LIST = LIST.filter((item) => (item.id !== Number(toRemoveFromLocalStorage[j])));
+    }
+    //for loop to change id's according to index position 
+    for (let k = 0; k < LIST.length; k++) {
+        LIST[k].id = k;
+    }
+    refreshEventListeners();
+    updateDraggableId();
+    refreshList();
 });
 
 //click event to togle dark and light themes
@@ -173,33 +156,32 @@ input.addEventListener("keyup", () => {
             LIST.push({
                 name: todo,
                 id: id,
-                completed: false,
-                deleted: false
+                completed: false
             });
             id++;
             localStorage.setItem("todoList", JSON.stringify(LIST));
             counter();
         }
         input.value = "";
-        refreshDraggablesEvent();
+        refreshEventListeners();
     }
 });
 
 /*----------------FUNCTIONS------------*/
 /*function to make the list empty before adding chosen type of items */
 function emptyList() {
-    let filterAll = LIST.filter((item) => (item.deleted == false));
     let element;
-    filterAll.forEach((item) => {
+    LIST.forEach((item) => {
         element = document.getElementById(item.id);
         if (element) {
             element.parentNode.parentNode.removeChild(element.parentNode);
         }
     });
 }
-function changeFont(btnName){
-    console.log("hi")
-    switch(btnName){
+
+/*function to change font color of chosen filter (all, complete or active) */
+function changeFont(btnName) {
+    switch (btnName) {
         case "btnAll":
             btnAll.forEach((btn) => btn.classList.add(FONTACTIVE))
             btnActive.forEach((btn) => btn.classList.remove(FONTACTIVE))
@@ -219,19 +201,16 @@ function changeFont(btnName){
             break;
     }
 }
-//function to run each item from local storage a createTodo function
+// //function to run each item from local storage a createTodo function
 function loadList(LIST) {
     LIST.forEach((item) => {
-        createTodo(item.name, item.id, item.completed, item.deleted);
+        createTodo(item.name, item.id, item.completed);
     });
     counter();
 }
 
 /*function to create todo item on the list*/
-function createTodo(todo, id, completed, deleted) {
-    if (deleted) {
-        return;
-    }
+function createTodo(todo, id, completed) {
     let complete = completed ? COMPLETED : "";
     let line = completed ? LINE_THROUGH : "";
     let todoItem =
@@ -252,101 +231,118 @@ function completeTodo(element) {
     counter();
 }
 
+/*function to refresh list items after changes to local storage are made so that draggable items would
+ contain the newest changes. This is used for drag and drop to reorder items on screen and elements in 
+ local storage*/
+function refreshList() {
+    let filter = document.querySelector(".font-active").className.split(' ');
+    if (filter[1] == "filter-todo__btn-all") {
+        emptyList();
+        loadList(LIST);
+    } else if (filter[1] == "filter-todo__btn-active") {
+        let activeArr = LIST.filter((item) => (item.completed == false));
+        emptyList();
+        loadList(activeArr);
+    } else if (filter[1] == "filter-todo__btn-complete") {
+        let completedArr = LIST.filter((item) => (item.completed == true));
+        emptyList();
+        loadList(completedArr);
+    } else {
+        console.log("Some issues with finding filter class")
+    }
+    refreshEventListeners();
+    localStorage.setItem("todoList", JSON.stringify(LIST));
+}
+
 /*function to delete list items*/
 function deleteTodo(element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
-    LIST[element.id].deleted = true;
-    localStorage.setItem("todoList", JSON.stringify(LIST));
+    /*Element was removed to prevent error that occured after deleting list items and using drag/drop */
+    LIST.splice(element.id, 1)
+    for (let i = 0; i < LIST.length; i++) {
+        LIST[i].id = i;
+    }
+    id = LIST.length;
     counter();
+    refreshEventListeners();
+    updateDraggableId();
+    refreshList();
 }
 
 /*function to display the number of items on the list*/
 function counter() {
-    let itemsLeft = LIST.filter((item) => (item.deleted == false && item.completed == false)).length;
+    let itemsLeft = LIST.filter((item) => (item.completed == false)).length;
     counterItemsLeft.innerHTML = `${itemsLeft}`;
 }
 
 /*------DRAG 'N DROP--------*/
-
-/**/
-let exp;
-let end = 0;
-let start = 0;
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add("dragging");
-        start = draggable.childNodes[0].id;
-    });
-    draggable.addEventListener("dragend", () => {
-        draggable.classList.remove("dragging");
-        // reorderItems(exp)
-        reorderItems();
-    });
-})
-
-function reorderItems(){
-    console.log(start, '-start', end, "-end", "arr-", idArr);
-    let skaiciuok = 0;
-    let tmp = new Object;
-    if(start > end){
+/*function to reorder items on the LIST by removing dragstart item then placing it
+in the dragend position. After this ids of LIST(local storage) elements are set according to 
+their index position
+*/
+function reorderItems() {
+    let tmp = [];
+    if (start > end) {
         tmp = LIST.splice(start, 1);
-        console.log(LIST)
         LIST.splice(Number(end) + 1, 0, tmp[0]);
-    }else if(start < end){
+    } else if (start < end) {
         tmp = LIST.splice(start, 1);
-        console.log(LIST)
         LIST.splice(Number(end), 0, tmp[0]);
     }
-    for(let i = 0; i < LIST.length; i++){
+    LIST = LIST.filter((item) => item !== undefined);
+    for (let i = 0; i < LIST.length; i++) {
         LIST[i].id = i;
-        console.log(idArr[i])
-        // console.log(LIST[idArr[i]].id)
     }
-    refreshDraggablesEvent();
-    updateId();
-    localStorage.setItem("todoList", JSON.stringify(LIST));
-    let tamo = document.querySelector(".font-active").className.split(' ');
-    console.log(tamo)
-    if(tamo[1] == "filter-todo__btn-all"){
-        let allArr = LIST.filter((item) => (item.deleted == false));
-        emptyList();
-        loadList(allArr);
-        refreshDraggablesEvent();
-    }else if(tamo[1] == "filter-todo__btn-active"){
-        let activeArr = LIST.filter((item) => (item.completed == false) && (item.deleted == false));
-        emptyList();
-        loadList(activeArr);
-        refreshDraggablesEvent();
-    }else if(tamo[1] == "filter-todo__btn-complete"){
-        let activeArr = LIST.filter((item) => (item.completed == true) && (item.deleted == false));
-        emptyList();
-        loadList(activeArr);
-        refreshDraggablesEvent();
-    }else{
-        console.log("Some issues with filter")
-    }
-    
+    refreshEventListeners();
+    refreshList();
 }
-function updateId(){
-    let l = LIST.length;
-    l--;
-    for(let k = 0; k < listItems.length - 1; k++){
-        console.log(listItems[k].childNodes[0].id,  "before")
-        listItems[k].childNodes[0].id = l;
-        listItems[k].childNodes[3].id = l;
-        console.log(listItems[k].childNodes[0].id, "after")
-        console.log(l)
 
-        l--;
+/*function to update Ids of draggables. Which allows to prevent errors after items are removed from the
+list*/
+function updateDraggableId() {
+    let i = LIST.length;
+    i--;
+    for (let k = 0; k < listItems.length - 1; k++) {
+        listItems[k].childNodes[0].id = i;
+        listItems[k].childNodes[3].id = i;
+        i--;
     }
 }
+
+/*function that refreshes draggables and allows dragstart/mouseover(out) to function w/o errors
+ after updates of list items (filtered or added)*/
+function refreshEventListeners() {
+    draggables = document.querySelectorAll('.draggable');
+    listItems = document.querySelectorAll(".list-todo__li");
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', () => {
+            draggable.classList.add("dragging");
+            start = draggable.childNodes[0].id;
+        });
+        draggable.addEventListener("dragend", () => {
+            draggable.classList.remove("dragging");
+            reorderItems();
+        });
+    })
+    draggables.forEach((draggable) => {
+        draggable.addEventListener("mouseover", () => {
+            draggable.classList.remove("mouseover")
+        })
+    })
+    draggables.forEach((draggable) => {
+        draggable.addEventListener("mouseout", () => {
+            draggable.classList.add("mouseover")
+        })
+    })
+}
+
 listTodo.addEventListener("dragover", (e) => {
     e.preventDefault();
     const afterElement = getDragAfterElement(listTodo, e.clientY);
     let draggable = document.querySelector(".dragging");
-    if(afterElement){
+    if (afterElement) {
         end = afterElement.childNodes[0].id;
-    }else{
+    } else {
         end = -1;
     }
     if (afterElement == undefined) {
@@ -374,32 +370,4 @@ function getDragAfterElement(container, y) {
     }, {
         offset: Number.NEGATIVE_INFINITY
     }).element;
-}
-
-
-/*function that refreshes draggables and allows dragstart/mouseover(out) to function w/o errors after updates of list items (filtered or added)*/
-function refreshDraggablesEvent() {
-    draggables = document.querySelectorAll('.draggable');
-    listItems = document.querySelectorAll(".list-todo__li");
-    console.log(draggables)
-    draggables.forEach(draggable => {
-        draggable.addEventListener('dragstart', () => {
-            draggable.classList.add("dragging");
-            start = draggable.childNodes[0].id;
-        });
-        draggable.addEventListener("dragend", () => {
-            draggable.classList.remove("dragging");
-            reorderItems();
-        });
-    })
-    draggables.forEach((draggable) => {
-        draggable.addEventListener("mouseover", () => {
-            draggable.classList.remove("mouseover")
-        })
-    })
-    draggables.forEach((draggable) => {
-        draggable.addEventListener("mouseout", () => {
-            draggable.classList.add("mouseover")
-        })
-    })
 }
